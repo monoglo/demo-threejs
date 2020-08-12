7495cef13f5c7a0b2fa06ba6d52c213229e75e7f
monoglo <monoglo37@gmail.com>
2020-08-12 17:45:53 +0800

    chore(three): load on demand

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import * as three from 'three'
 
 Vue.config.productionTip = false
-Vue.prototype.$three = three
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
 
 <script>
 // @ is an alias to /src
-
+import * as THREE from 'three'
 export default {
   name: 'Home',
   data: () => ({
@@ -17,22 +17,22 @@ export default {
   },
   methods: {
     initThreejs() {
-      var scene = new this.$three.Scene()
-      var camera = new this.$three.PerspectiveCamera(
+      var scene = new THREE.Scene()
+      var camera = new THREE.PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
         0.1,
         1000
       )
 
-      var renderer = new this.$three.WebGLRenderer()
-      renderer.setSize(window.innerWidth, window.innerHeight)
+      var renderer = new THREE.WebGLRenderer()
+      renderer.setSize(window.innerWidth / 2, window.innerHeight / 2)
       document.body.appendChild(renderer.domElement)
-      var geometry = new this.$three.BoxGeometry(1, 1, 1)
-      var material = new this.$three.MeshBasicMaterial({
+      var geometry = new THREE.BoxGeometry(1, 1, 1)
+      var material = new THREE.MeshBasicMaterial({
         color: 0x00ff00
       })
-      var cube = new this.$three.Mesh(geometry, material)
+      var cube = new THREE.Mesh(geometry, material)
       scene.add(cube)
 
       camera.position.z = 5
@@ -40,7 +40,7 @@ export default {
       function animate() {
         requestAnimationFrame(animate)
         cube.rotation.x += 0.01
-        cube.rotation.y += 0.01
+        cube.rotation.y += 0.02
         renderer.render(scene, camera)
       }
       animate()
