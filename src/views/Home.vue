089f2bc515d6a2c7fec56055b7e46328ef68c654
monoglo <monoglo37@gmail.com>
2020-08-12 17:25:09 +0800

    demo(Home): create threejs demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+    msg: 'helloworld'
+  }),
+  created() {
+    this.initThreejs()
+  },
+  methods: {
+    initThreejs() {
+      var scene = new this.$three.Scene()
+      var camera = new this.$three.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      )
+
+      var renderer = new this.$three.WebGLRenderer()
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      document.body.appendChild(renderer.domElement)
+      var geometry = new this.$three.BoxGeometry(1, 1, 1)
+      var material = new this.$three.MeshBasicMaterial({
+        color: 0x00ff00
+      })
+      var cube = new this.$three.Mesh(geometry, material)
+      scene.add(cube)
+
+      camera.position.z = 5
+
+      function animate() {
+        requestAnimationFrame(animate)
+        cube.rotation.x += 0.01
+        cube.rotation.y += 0.01
+        renderer.render(scene, camera)
+      }
+      animate()
+    }
   }
 }
 </script>
