1660cca3f2a32d100db9f0c4a62e845b4bbbcf1c
monoglo <monoglo37@gmail.com>
2020-08-12 17:24:35 +0800

    chore(env): config eslint and prettier

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as three from 'three'
 
 Vue.config.productionTip = false
+Vue.prototype.$three = three
 
 new Vue({
   router,
