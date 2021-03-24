5c989770865e86e67648171b35ec5ea910a9bec3
monoglo <monoglo37@gmail.com>
2021-03-24 17:50:27 +0800

    feat(lab): arrow

--- src/views/Lab.vue
+++ src/views/Lab.vue
@@ -49,7 +49,8 @@ export default {
     stats: null,
     lableDiv: null,
     nameLabel: null,
-    famenzhuan: false
+    famenzhuan: false,
+    arrowMoved: 0
   }),
   mounted() {
     this.initThreejs()
@@ -240,6 +241,14 @@ export default {
       this.stats.begin()
       // this.renderer.render(this.scene, this.camera)
       this.orbitControls.update()
+      if (this.scene.getObjectByName('箭头')) {
+        this.scene.getObjectByName('箭头').position.x += 0.02
+        this.arrowMoved += 0.02
+        if (this.arrowMoved >= 2) {
+          this.arrowMoved = 0
+          this.scene.getObjectByName('箭头').position.x -= 2
+        }
+      }
       if (this.lableDiv) {
         if (this.count > 120) {
           this.lableDiv.textContent =
@@ -321,133 +330,133 @@ export default {
       this.scene.getObjectById(813).visible = !this.scene.getObjectById(813)
         .visible
       console.info(this.dragControls.getObjects())
-    },
-    roundRect(ctx, x, y, w, h, r) {
-      ctx.beginPath()
-      ctx.moveTo(x + r, y)
-      ctx.lineTo(x + w - r, y)
-      ctx.quadraticCurveTo(x + w, y, x + w, y + r)
-      ctx.lineTo(x + w, y + h - r)
-      ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h)
-      ctx.lineTo(x + r, y + h)
-      ctx.quadraticCurveTo(x, y + h, x, y + h - r)
-      ctx.lineTo(x, y + r)
-      ctx.quadraticCurveTo(x, y, x + r, y)
-      ctx.closePath()
-      ctx.fill()
-      ctx.stroke()
-    },
-    makeTextSpriteMaterial(message, parameters) {
-      if (parameters === undefined) parameters = {}
-      var fontface = Object.prototype.hasOwnProperty.call(
-        parameters,
-        'fontface'
-      )
-        ? parameters.fontface
-        : 'Arial'
-      var fontsize = Object.prototype.hasOwnProperty.call(
-        parameters,
-        'fontsize'
-      )
-        ? parameters.fontsize
-        : 18
-      var borderThickness = Object.prototype.hasOwnProperty.call(
-        parameters,
-        'borderThickness'
-      )
-        ? parameters.borderThickness
-        : 4
-      var borderColor = Object.prototype.hasOwnProperty.call(
-        parameters,
-        'borderColor'
-      )
-        ? parameters.borderColor
-        : { r: 0, g: 0, b: 0, a: 1.0 }
-      var backgroundColor = Object.prototype.hasOwnProperty.call(
-        parameters,
-        'backgroundColor'
-      )
-        ? parameters.backgroundColor
-        : { r: 255, g: 255, b: 255, a: 1.0 }
-      var textColor = Object.prototype.hasOwnProperty.call(
-        parameters,
-        'textColor'
-      )
-        ? parameters.textColor
-        : { r: 0, g: 0, b: 0, a: 1.0 }
+      // },
+      // roundRect(ctx, x, y, w, h, r) {
+      //   ctx.beginPath()
+      //   ctx.moveTo(x + r, y)
+      //   ctx.lineTo(x + w - r, y)
+      //   ctx.quadraticCurveTo(x + w, y, x + w, y + r)
+      //   ctx.lineTo(x + w, y + h - r)
+      //   ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h)
+      //   ctx.lineTo(x + r, y + h)
+      //   ctx.quadraticCurveTo(x, y + h, x, y + h - r)
+      //   ctx.lineTo(x, y + r)
+      //   ctx.quadraticCurveTo(x, y, x + r, y)
+      //   ctx.closePath()
+      //   ctx.fill()
+      //   ctx.stroke()
+      // },
+      // makeTextSpriteMaterial(message, parameters) {
+      //   if (parameters === undefined) parameters = {}
+      //   var fontface = Object.prototype.hasOwnProperty.call(
+      //     parameters,
+      //     'fontface'
+      //   )
+      //     ? parameters.fontface
+      //     : 'Arial'
+      //   var fontsize = Object.prototype.hasOwnProperty.call(
+      //     parameters,
+      //     'fontsize'
+      //   )
+      //     ? parameters.fontsize
+      //     : 18
+      //   var borderThickness = Object.prototype.hasOwnProperty.call(
+      //     parameters,
+      //     'borderThickness'
+      //   )
+      //     ? parameters.borderThickness
+      //     : 4
+      //   var borderColor = Object.prototype.hasOwnProperty.call(
+      //     parameters,
+      //     'borderColor'
+      //   )
+      //     ? parameters.borderColor
+      //     : { r: 0, g: 0, b: 0, a: 1.0 }
+      //   var backgroundColor = Object.prototype.hasOwnProperty.call(
+      //     parameters,
+      //     'backgroundColor'
+      //   )
+      //     ? parameters.backgroundColor
+      //     : { r: 255, g: 255, b: 255, a: 1.0 }
+      //   var textColor = Object.prototype.hasOwnProperty.call(
+      //     parameters,
+      //     'textColor'
+      //   )
+      //     ? parameters.textColor
+      //     : { r: 0, g: 0, b: 0, a: 1.0 }
 
-      var canvas = document.createElement('canvas')
-      var context = canvas.getContext('2d')
-      context.font = 'Bold ' + fontsize + 'px ' + fontface
-      // get size data (height depends only on font size)
-      var metrics = context.measureText(message)
-      var textWidth = metrics.width
+      //   var canvas = document.createElement('canvas')
+      //   var context = canvas.getContext('2d')
+      //   context.font = 'Bold ' + fontsize + 'px ' + fontface
+      //   // get size data (height depends only on font size)
+      //   var metrics = context.measureText(message)
+      //   var textWidth = metrics.width
 
-      // background color
-      context.fillStyle =
-        'rgba(' +
-        backgroundColor.r +
-        ',' +
-        backgroundColor.g +
-        ',' +
-        backgroundColor.b +
-        ',' +
-        backgroundColor.a +
-        ')'
-      // border color
-      context.strokeStyle =
-        'rgba(' +
-        borderColor.r +
-        ',' +
-        borderColor.g +
-        ',' +
-        borderColor.b +
-        ',' +
-        borderColor.a +
-        ')'
+      //   // background color
+      //   context.fillStyle =
+      //     'rgba(' +
+      //     backgroundColor.r +
+      //     ',' +
+      //     backgroundColor.g +
+      //     ',' +
+      //     backgroundColor.b +
+      //     ',' +
+      //     backgroundColor.a +
+      //     ')'
+      //   // border color
+      //   context.strokeStyle =
+      //     'rgba(' +
+      //     borderColor.r +
+      //     ',' +
+      //     borderColor.g +
+      //     ',' +
+      //     borderColor.b +
+      //     ',' +
+      //     borderColor.a +
+      //     ')'
 
-      context.lineWidth = borderThickness
-      this.roundRect(
-        context,
-        borderThickness / 2,
-        borderThickness / 2,
-        textWidth + borderThickness,
-        fontsize * 1.4 + borderThickness,
-        6
-      )
-      // 1.4 is extra height factor for text below baseline: g,j,p,q.
-      // text color
+      //   context.lineWidth = borderThickness
+      //   this.roundRect(
+      //     context,
+      //     borderThickness / 2,
+      //     borderThickness / 2,
+      //     textWidth + borderThickness,
+      //     fontsize * 1.4 + borderThickness,
+      //     6
+      //   )
+      //   // 1.4 is extra height factor for text below baseline: g,j,p,q.
+      //   // text color
 
-      context.fillStyle =
-        'rgba(' +
-        textColor.r +
-        ', ' +
-        textColor.g +
-        ', ' +
-        textColor.b +
-        ', 1.0)'
-      context.fillText(message, borderThickness, fontsize + borderThickness)
+      //   context.fillStyle =
+      //     'rgba(' +
+      //     textColor.r +
+      //     ', ' +
+      //     textColor.g +
+      //     ', ' +
+      //     textColor.b +
+      //     ', 1.0)'
+      //   context.fillText(message, borderThickness, fontsize + borderThickness)
 
-      // canvas contents will be used for a texture
-      var texture = new THREE.Texture(canvas)
-      texture.needsUpdate = true
+      //   // canvas contents will be used for a texture
+      //   var texture = new THREE.Texture(canvas)
+      //   texture.needsUpdate = true
 
-      return new THREE.SpriteMaterial({
-        map: texture
-      })
-    },
-    makeTextSprite(message, parameters) {
-      var sprite = new THREE.Sprite(
-        this.makeTextSpriteMaterial(message, parameters)
-      )
-      var fontsize = Object.prototype.hasOwnProperty.call(
-        parameters,
-        'fontsize'
-      )
-        ? parameters.fontsize
-        : 18
-      sprite.scale.set(0.05 * fontsize, 0.025 * fontsize, 0.01)
-      return sprite
+      //   return new THREE.SpriteMaterial({
+      //     map: texture
+      //   })
+      // },
+      // makeTextSprite(message, parameters) {
+      //   var sprite = new THREE.Sprite(
+      //     this.makeTextSpriteMaterial(message, parameters)
+      //   )
+      //   var fontsize = Object.prototype.hasOwnProperty.call(
+      //     parameters,
+      //     'fontsize'
+      //   )
+      //     ? parameters.fontsize
+      //     : 18
+      //   sprite.scale.set(0.05 * fontsize, 0.025 * fontsize, 0.01)
+      //   return sprite
     }
   }
 }
